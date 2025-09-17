244b62e10a454b178858b0527ae92495943bdca0
Brunofantoli <brunofantoli@gmail.com>
2025-09-17 15:30:19 +0000

    changed the variable_id to the one created by Ysaline

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 
 # List of (ean, variable_id) pairs
 EAN_VARIABLE_PAIRS = [
-    ("541448820055391175", 7185058),
+    ("541448820055391175", 6129325),
     ("541448820055391069", 6129353),
     # Add more pairs as needed
     # ("EAN_NUMBER", VARIABLE_ID),
